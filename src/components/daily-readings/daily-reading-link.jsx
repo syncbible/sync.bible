f1460b73eaa15357f2082f226e562f51bd9ad751
borealsole <99179214+borealsole@users.noreply.github.com>
2025-05-22 23:00:00 +0100

    translate book names for readings

--- src/components/daily-readings/daily-reading-link.jsx
+++ src/components/daily-readings/daily-reading-link.jsx
@@ -1,15 +1,22 @@
 // External dependencies
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 // Internal dependencies
 import { goToReferenceAction } from '../../actions';
+import bible from '../../data/bible.js';
 
 /** Link component for daily Bible readings */
 export default function DailyReadingLink( { book, chapter, verses } ) {
 	const dispatch = useDispatch();
+	const interfaceLanguage = useSelector(
+		( state ) => { return state.settings.interfaceLanguage }
+	);
 
-	let referenceString = book + ' ' + chapter;
+	const translatedBook = bible.getTranslatedBookName( book, interfaceLanguage );
+
+	let referenceString = translatedBook + ' ' + chapter;
 	const reference = { book, chapter };
 
 	if ( verses ) {

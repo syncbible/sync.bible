80dc422c962b7f134993065540cf68795d46f807
Ben Dwyer <ben@scruffian.com>
2025-06-16 17:48:09 +0100

    update number of translations

--- src/data/bible.js
+++ src/data/bible.js
@@ -15991,5 +15991,6 @@ bible.isRtlVersion = function ( version, book ) {
 		return true;
 	}
 };
-
+console.log( Object.keys( bible.Data.supportedVersions ) );
+console.log( Object.keys( bible.Data.tongues ) );
 export default bible;

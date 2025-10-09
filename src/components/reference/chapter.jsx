d29a18d16a8395cf8cabfb90ee3014de0b6d2521
Ben Dwyer <ben@scruffian.com>
2025-10-09 14:26:27 +0100

    remove findDOMNode

--- src/components/reference/chapter.jsx
+++ src/components/reference/chapter.jsx
@@ -67,7 +67,7 @@ const Chapter = ( { book, chapter, index } ) => {
 	] );
 
 	const scrollToCurrentChapter = () => {
-		const currrentChapter = ReactDOM.findDOMNode( currentRef.current );
+		const currrentChapter = currentRef.current;
 		const referenceWindow = document.getElementById(
 			'referenceWindow' + index
 		);

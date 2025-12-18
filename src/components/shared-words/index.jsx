a6f445cf2d905d787d440f516ed5a8ffa6925c8c
Ben Dwyer <ben@scruffian.com>
2025-12-19 09:47:54 +1300

    make it possible to sort by strongs number

--- src/components/shared-words/index.jsx
+++ src/components/shared-words/index.jsx
@@ -1,6 +1,7 @@
 // External dependencies
 import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
 
 // Internal dependencies
 import { fetchData } from '../../actions/index.js';
@@ -12,8 +13,9 @@ import {
 import Lemma from '../lemma';
 import styles from './styles.module.scss';
 
-export default function SharedWords( { type } ) {
+function SharedWords( { type } ) {
 	const [ limit, setLimit ] = useState( 100 );
+	const [ sortBy, setSortBy ] = useState( 'count-desc' );
 	const list = useSelector( ( state ) => state.list );
 	const listOfReferences = getListOfReferencesFromType( list, type );
 	const dispatch = useDispatch();
@@ -32,9 +34,21 @@ export default function SharedWords( { type } ) {
 
 	const [ open, setOpen ] = useState( false );
 
+	const sortWords = ( wordA, wordB ) => {
+		if ( sortBy === 'count-desc' ) {
+			return sharedWords[ wordB ] - sharedWords[ wordA ];
+		} else if ( sortBy === 'strongs' ) {
+			// Sort by Strong's number (numeric comparison)
+			const numA = parseInt( wordA.substring( 1 ) );
+			const numB = parseInt( wordB.substring( 1 ) );
+			return numA - numB;
+		}
+		return 0;
+	};
+
 	const sharedWordsRendered = Object.keys( sharedWords )
 		.filter( ( word ) => sharedWords[ word ] > 1 )
-		.sort( ( wordA, wordB ) => sharedWords[ wordB ] > sharedWords[ wordA ] )
+		.sort( sortWords )
 		.map( ( word, index ) => (
 			<li key={ index }>
 				<Lemma
@@ -67,9 +81,26 @@ export default function SharedWords( { type } ) {
 				className={ styles.limit }
 			/>{ ' ' }
 			uses
+			<br />
+			<label htmlFor="shared-words-sort">Sort by:</label>{ ' ' }
+			<select
+				id="shared-words-sort"
+				value={ sortBy }
+				onChange={ ( event ) => setSortBy( event.target.value ) }
+				className={ styles.select }
+			>
+				<option value="count-desc">Uses</option>
+				<option value="strongs">Strong&apos;s number</option>
+			</select>
 			{ sharedWordsRendered.length > 0 && (
 				<ol>{ sharedWordsRendered }</ol>
 			) }
 		</Collapsible>
 	) : null;
 }
+
+SharedWords.propTypes = {
+	type: PropTypes.string.isRequired,
+};
+
+export default SharedWords;

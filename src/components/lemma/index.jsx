d4a62cad5579cf0fc3c4b13e9e6dfe6f5cc9978e
Ben Dwyer <ben@scruffian.com>
2025-12-19 17:26:23 +1300

    add significance to the title text

--- src/components/lemma/index.jsx
+++ src/components/lemma/index.jsx
@@ -1,17 +1,29 @@
 // External dependencies
 import { useDispatch, useSelector } from 'react-redux';
 import classnames from 'classnames';
+import PropTypes from 'prop-types';
 
 // Internal dependencies
 import { selectWord } from '../../actions';
 import stripPointing from '../../lib/strip-pointing.js';
 import styles from './styles.module.scss';
 
-export default function Lemma( { lemma, version, count } ) {
+function Lemma( { lemma, version, count } ) {
 	const dispatch = useDispatch();
 	const strongsDictionary = useSelector(
 		( state ) => state.data.strongsDictionary
 	);
+	const strongsObjectWithFamilies = useSelector(
+		( state ) => state.data.strongsObjectWithFamilies
+	);
+
+	// Calculate title text with significance if count is provided
+	let titleText = strongsDictionary && strongsDictionary[ lemma ].xlit;
+	if ( count && strongsObjectWithFamilies?.[ lemma ]?.count ) {
+		const totalUses = strongsObjectWithFamilies[ lemma ].count;
+		const significance = ( count / totalUses ).toFixed( 3 );
+		titleText = `${ titleText }\nUses in references: ${ count }, Total uses: ${ totalUses }, Significance: ${ significance }`;
+	}
 
 	return (
 		<a
@@ -25,7 +37,7 @@ export default function Lemma( { lemma, version, count } ) {
 			onClick={ () => {
 				dispatch( selectWord( { lemma, version } ) );
 			} }
-			title={ strongsDictionary && strongsDictionary[ lemma ].xlit }
+			title={ titleText }
 		>
 			<span>{ lemma }</span>
 			<span>
@@ -36,3 +48,11 @@ export default function Lemma( { lemma, version, count } ) {
 		</a>
 	);
 }
+
+Lemma.propTypes = {
+	lemma: PropTypes.string.isRequired,
+	version: PropTypes.string.isRequired,
+	count: PropTypes.number,
+};
+
+export default Lemma;
